6658656d963cdad0648366ab0c120f5aa82d1dbe
Omneya Osman <engomneya.osman@gmail.com>
2023-04-30 03:20:07 +0300

    all tasks done UI

--- app/src/main/java/com/omni/composecamp/MainActivity.kt
+++ app/src/main/java/com/omni/composecamp/MainActivity.kt
@@ -3,17 +3,22 @@ package com.omni.composecamp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.omni.composecamp.ui.theme.ComposeCampTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AllTasksDone("All Tasks Completed", "Nice Work")
                 }
             }
         }
@@ -34,19 +39,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Magenta) {
-        Text(
-            text = "Hello my name is $name!",
-            modifier = modifier.padding(8.dp)
+fun AllTasksDone(tasksStatus: String, greeting: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = "All tasks done"
         )
+        Text(
+            text = tasksStatus,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp , bottom = 8.dp)
+        )
+        Text(text = greeting , fontSize = 16.sp)
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ComposeCampTheme {
-        Greeting("Android")
+        AllTasksDone("All Tasks Completed", "Nice Work")
     }
 }
