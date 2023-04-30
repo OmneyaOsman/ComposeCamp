7758d740241f53c51ef31a840c770933590ec080
Omneya Osman <engomneya.osman@gmail.com>
2023-04-30 04:21:36 +0300

    quadrant

--- app/src/main/java/com/omni/composecamp/MainActivity.kt
+++ app/src/main/java/com/omni/composecamp/MainActivity.kt
@@ -3,15 +3,23 @@ package com.omni.composecamp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.omni.composecamp.ui.theme.ComposeCampTheme
@@ -26,27 +34,77 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeInfo()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Magenta) {
-        Text(
-            text = "Hello my name is $name!",
-            modifier = modifier.padding(8.dp)
-        )
+fun ComposeInfo() {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                stringResource(id = R.string.first_title),
+                stringResource(id = R.string.first_description), Color.Green, Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                stringResource(id = R.string.third_title),
+                stringResource(id = R.string.third_description), Color.Yellow, Modifier.weight(1f)
+            )
+        }
+
+        Row(Modifier.weight(1f)) {
+
+            ComposableInfoCard(
+                stringResource(id = R.string.second_title),
+                stringResource(id = R.string.second_description), Color.Cyan, Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                stringResource(id = R.string.fourth_title),
+                stringResource(id = R.string.fourth_description),
+                Color.LightGray,
+                Modifier.weight(1f)
+            )
+        }
     }
+
 }
 
-@Preview(showBackground = true)
+@Composable
+fun ComposableInfoCard(
+    name: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = name,
+            modifier = Modifier.padding(bottom = 16.dp), fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify,
+        )
+    }
+
+}
+
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ComposeCampTheme {
-        Greeting("Android")
+        ComposeInfo()
     }
 }
