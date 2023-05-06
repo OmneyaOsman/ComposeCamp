0d8c64fde4e869db681bd6649ba0ce498a512fd6
Omneya Osman <engomneya.osman@gmail.com>
2023-05-06 23:03:23 +0300

    create animated list

--- app/src/main/java/com/omni/composecamp/MainActivity.kt
+++ app/src/main/java/com/omni/composecamp/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.omni.composecamp
 
-import android.content.res.Configuration
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,20 +18,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.omni.composecamp.ui.theme.ComposeCampTheme
-import java.nio.file.WatchEvent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,7 +45,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -68,7 +71,13 @@ fun MessageCard(message: Message, modifier: Modifier = Modifier) {
                 .border(BorderStroke(1.5.dp, MaterialTheme.colorScheme.primary))
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = message.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -76,9 +85,17 @@ fun MessageCard(message: Message, modifier: Modifier = Modifier) {
             )
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp),
+                color = surfaceColor,
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp
+            ) {
                 Text(
                     text = message.body,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(4.dp)
                 )
@@ -87,13 +104,22 @@ fun MessageCard(message: Message, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(name ="Light mode")
-@Preview(name ="Dark mode" , uiMode = UI_MODE_NIGHT_YES , showBackground = true )
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
+    }
+}
+
+@Preview(name = "Light mode")
+@Preview(name = "Dark mode", uiMode = UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun MessageCardPreview() {
     ComposeCampTheme {
         Surface {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            Conversation(SampleData.conversationSample)
         }
     }
 }
